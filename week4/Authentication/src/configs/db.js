11735f4ad4ecdf098012ec3f1a6325eb7211ae36
Tejaswini Khengare <97454567+Tejaswini179@users.noreply.github.com>
2022-03-24 19:59:29 +0530

    commit

---
+++ week4/Authentication/src/configs/db.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    return mongoose.connect("mongodb+srv://Tejaswini:Tejaswini@cluster0.0kblt.mongodb.net/ONLINEDB?retryWrites=true&w=majority");
+};
